f04a88183581e72988161f4906a08987b0518557
Samgrass <samuelegrassetti01@gmail.com>
2024-09-11 20:56:53 +0200

    FINE ESERCIZIO

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,36 @@
 {{-- questa view estende il file main.blade.php che è dentro la cartella view/layouts --}}
 @extends('layouts.main')
 
-@section('hero')
-    <div>HERO HOME</div>
-@endsection
 
 @section('content')
 <div class="container my-5">
-    <h1>{{ $title }}</h1>
-    <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
-    <p>
-        {{ $text }}
-    </p>
+    <h1>Lista film</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Titolo originale</th>
+                <th scope="col">Anno di uscita</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($movies as $movie)
+            <tr>
+                <th>{{ $movie->id }}</th>
+                <td>{{ $movie->title }}</td>
+                <td>{{ $movie->original_title }}</td>
+                <td>{{ $movie->date }}</td>
+            </tr>
+
+            @endforeach
+        </tbody>
+    </table>
 </div>
 
 @endsection
 
 
 @section('titlePage')
-    home
+home
 @endsection
